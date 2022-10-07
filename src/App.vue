522da80b8797ec3b436663c13f465046f5886fec
zhimingo <huangzhimingme@gmail.com>
2022-10-07 18:26:08 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <n-config-provider :theme-overrides="themeOverride">
+    <n-theme-editor>
+      <n-button type="primary">Button</n-button>
+      <n-button type="info">Button</n-button>
+      <n-button type="success">Button</n-button>
+      <n-button type="warning">Button</n-button>
+      <n-button type="error">Button</n-button>
+    </n-theme-editor>
+  </n-config-provider>
+</template>
+
+<script setup>
+import { NButton, NConfigProvider, NThemeEditor } from 'naive-ui';
+import { reactive } from 'vue';
+const themeOverride = reactive({
+  common: {
+    primaryColor: '#2d8cf0',
+    primaryColorHover: '#5cadff',
+    primaryColorPressed: '#2b85e4',
+    primaryColorSuppl: '#5cadff',
+    infoColor: '#2db7f5',
+    infoColorHover: '#57c5f7',
+    infoColorPressed: '#2daee9',
+    infoColorSuppl: '#57c5f7',
+    successColor: '#19be6b',
+    successColorHover: '#47cb89',
+    successColorPressed: '#18b566',
+    successColorSuppl: '#47cb89',
+    warningColor: '#ff9900',
+    warningColorHover: '#ffad33',
+    warningColorPressed: '#f29100',
+    warningColorSuppl: '#ffad33',
+    errorColor: '#ed4014',
+    errorColorHover: '#f16643',
+    errorColorPressed: '#e13d13',
+    errorColorSuppl: '#f16643',
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import 'normalize.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.css';
+
+createApp(App).mount('#app');
